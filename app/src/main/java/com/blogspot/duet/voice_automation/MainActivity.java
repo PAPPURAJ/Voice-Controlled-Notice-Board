3bd87673ba04251e96b9e186738534dda3301501
PAPPURAJ <pappuraj.duet@gmail.com>
2022-10-09 21:24:53 +0600

    Init

---
+++ app/src/main/java/com/blogspot/duet/voice_automation/MainActivity.java
@@ -0,0 +1,94 @@
+package com.blogspot.duet.voice_automation;
+
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.speech.RecognizerIntent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+    private final int REQ_CODE = 100;
+    EditText editText;
+    FirebaseDatabase firebaseDatabase=FirebaseDatabase.getInstance();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ImageView speak = findViewById(R.id.iv_mic);
+        editText=findViewById(R.id.myEt);
+
+
+
+        speak.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                        RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+                intent.putExtra(RecognizerIntent.EXTRA_PROMPT,
+                        "Need to speak");
+                try {
+                    startActivityForResult(intent, REQ_CODE);
+                } catch (ActivityNotFoundException a) {
+                    Toast.makeText(getApplicationContext(),
+                            "Sorry your device not supported",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case REQ_CODE: {
+                if (resultCode == RESULT_OK && null != data) {
+
+                    ArrayList<String> result = data
+                            .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+
+                    ((TextView) findViewById(R.id.tv_speech_to_text)).setText(result.get(0) + "");
+
+                    String text = result.get(0).toLowerCase();
+                    String name=editText.getText().toString();
+
+                    if(name.isEmpty()){
+                        Toast.makeText(getApplicationContext(),"Please input name",Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+
+                    firebaseDatabase.getReference("Notice").setValue(name+"="+text).addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void unused) {
+                            Toast.makeText(getApplicationContext(),"Notice Updated",Toast.LENGTH_SHORT).show();
+                        }
+                    });
+
+
+
+
+                }
+                break;
+            }
+        }
+    }
+}
